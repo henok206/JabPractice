bf29b4623c1288bf80aca92ec153ae1fb792aeed
Henok <henok_206@yahoo.com>
2023-02-22 13:15:19 +0300

    making change css

---
+++ changing_css/app.js
@@ -0,0 +1,18 @@
+let bodyEl = document.querySelector("body");
+let divEl = document.querySelector("div");
+let aEle = document.querySelector("a");
+
+bodyEl.style.backgroundColor = "#f0f0f2";
+bodyEl.style.marign = "0";
+bodyEl.style.padding = "0";
+bodyEl.style.fontFamily = "sans-serif";
+
+divEl.style.width = "600px";
+divEl.style.padding = "2em";
+divEl.style.margin = "5em auto";
+divEl.style.backgroundColor = "#fdfdff";
+divEl.style.borderRadius = "0.5em";
+divEl.style.boxShadow = "2px 3px 7px 2px rgba(0,0,0,0.02)";
+
+aEle.style.color = "#38488f";
+aEle.style.textDecoration = "none";
