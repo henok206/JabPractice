37e8937ae11b24c1f846df256f850fbcad294353
Henok <henok_206@yahoo.com>
2023-02-22 18:22:04 +0300

    made Dom_travers

---
+++ Dom_Traversal/app.js
@@ -0,0 +1,6 @@
+// let todoEl = document.querySelector(".todo");
+// let todosEl = document.getElementsByClassName("todo__item");
+
+//console.log(todoEl.parentNode);
+//console.log(todosEl[0].parentNode);
+//console.log(todosEl.firstElementChild);
